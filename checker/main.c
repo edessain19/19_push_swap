7fa5b3379a3f02f6f60e7a52114a871e1f3cb0de
Evrard Dessain <edessain@mi-c2-p8.s19.be>
2021-03-24 13:38:04 +0100

    first push

--- checker/main.c
+++ checker/main.c
@@ -1,29 +1,32 @@
 #include "../include/checker.h"
 
-// int     checker(int **tab)
-// {
+int     checker(int *tab)
+{
+    
 
-
-//     return (0);
-// }
+    return (0);
+}
 
 int     main(int argc, char **argv)
 {
     int     *tab;
     int     i;
+    int     len;
 
     i = 0;
+    len = argc - 1;
     if (argc < 2)
         return (-1);
-    tab = malloc(sizeof(int) * argc);
+    tab = malloc(sizeof(int) * len);
     if (tab == NULL)
         return (-1);
-    while (i < argc - 1)
+    while (i < len)
     {
         tab[i] = ft_atoi(argv[i + 1]);
-        i++;
         printf("tab[%i] = %i\n", i, tab[i]);
+        i++;
     }
-    // checker(tab);
+    checker(tab);
+    
     return (0);
 }
